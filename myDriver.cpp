9a632e9237923612c75860600d4d09eb0909a368
m-dhiraj <mdhiraj@utexas.edu>
2019-11-11 19:52:22 -0600

    i think i done
    yeeees

--- myDriver.cpp
+++ myDriver.cpp
@@ -10,7 +10,7 @@
 using namespace std;
 int checkGameStatus(Player p1, Player p2, Deck d){
     cout<<" * ";
-    if(d.size()<0&&(p1.getBookSize()+p2.getBookSize()==26))
+    if((d.size()<0&&(p1.getBookSize()+p2.getBookSize()==26)))
         return 1;
     return 0;
 }
@@ -43,12 +43,10 @@ int main(int argc, char *argv[])
         file<<endl;
     }
     while(checkGameStatus(p1, p2, d)==0){
-        if(checkGameStatus(p1, p2, d)==0&&p1.getHandSize()>0){
+        if(p1.getHandSize()==0&&d.size()>=0)
+        p1.addCard(d.dealCard());
+        else if(checkGameStatus(p1, p2, d)==0&&p1.getHandSize()>0){
         do{
-            file<<p1.getName()<<"'s hand :"<<p1.showHand()<<endl;
-        file<<p2.getName()<<"'s hand :"<<p2.showHand()<<endl;
-        file<<p1.getName()<<"'s books :"<<p1.showBooks()<<endl;
-        file<<p2.getName()<<"'s books :"<<p2.showBooks()<<endl;
             c1=p1.chooseCardFromHand();
             file<<p1.getName()<<" asks - do you have a "<<c1.rankString(c1.getRank())<<endl;
             if(p2.cardInHand(c1)){
@@ -58,7 +56,7 @@ int main(int argc, char *argv[])
                 c2=p2.removeCardFromHand(c1);
                 p1.addCard(c2);
                 p1.checkHandForBook(c1,c2);
-                p2CONT=true;
+                p1CONT=true;
             }
             else{
                 file<<p2.getName()<<" says - Go Fish!"<<endl;
@@ -67,7 +65,8 @@ int main(int argc, char *argv[])
                 p1.addCard(c1);
                 file<<p1.getName()<<" draws a "<<c1.toString()<<endl;
                 file<<endl;}
-                p2CONT=false;
+                p1CONT=false;
+                p2CONT=true;
                 if(p1.checkHandForBook(c1,c2))
                 file<<p1.getName()<<" books the "<<c1.rankString(c1.getRank())<<endl;
                 file<<endl;
@@ -77,12 +76,10 @@ int main(int argc, char *argv[])
         
         
     }
-         if(checkGameStatus(p1, p2, d)==0&&p2.getHandSize()>0){
+     if(p2.getHandSize()==0&&d.size()>=0)
+        p2.addCard(d.dealCard());
+         else if(checkGameStatus(p1, p2, d)==0&&p2.getHandSize()>0){
         do{
-            file<<p1.getName()<<"'s hand :"<<p1.showHand()<<endl;
-        file<<p2.getName()<<"'s hand :"<<p2.showHand()<<endl;
-        file<<p1.getName()<<"'s books :"<<p1.showBooks()<<endl;
-        file<<p2.getName()<<"'s books :"<<p2.showBooks()<<endl;
             c1=p2.chooseCardFromHand();
             file<<p2.getName()<<" asks - do you have a "<<c1.rankString(c1.getRank())<<endl;
             if(p1.cardInHand(c1)){
@@ -102,6 +99,7 @@ int main(int argc, char *argv[])
                 file<<p2.getName()<<" draws a "<<c1.toString()<<endl;
                 file<<endl;}
                 p2CONT=false;
+                p1CONT=true;
                 if(p2.checkHandForBook(c1,c2))
                 file<<p2.getName()<<" books the "<<c1.rankString(c1.getRank())<<endl;
             }
@@ -117,13 +115,19 @@ int main(int argc, char *argv[])
         file<<p2.getName()<<"'s hand :"<<p2.showHand()<<endl;
         file<<p1.getName()<<"'s books :"<<p1.showBooks()<<endl;
         file<<p2.getName()<<"'s books :"<<p2.showBooks()<<endl;
-        file<<p1.getName()<<"'s books :"<<p1.getBookSize()<<endl;
-        file<<p2.getName()<<"'s books :"<<p2.getBookSize()<<endl;
+        file<<p1.getName()<<"'s book size :"<<p1.getBookSize()<<endl;
+        file<<p2.getName()<<"'s book size :"<<p2.getBookSize()<<endl;
         if(p1.getBookSize()>p2.getBookSize())
         file<<p1.getName()<<" WINS!"<<endl;
-        if(p2.getBookSize()>p2.getBookSize())
+        else if(p2.getBookSize()>p2.getBookSize())
         file<<p2.getName()<<" WINS!"<<endl;
-        cout<<p1.getName()<<"'s hand :"<<p1.getHandSize()<<endl;
+        else if(p1.getBookSize()==p2.getBookSize())
+        file<<"ITS A TIE!"<<endl;
+
+        cout<<endl;
+        cout<<p1.getName()<<"'s deck :"<<p1.getBookSize()<<endl;
+        cout<<p2.getName()<<"'s deck :"<<p2.getBookSize()<<endl;
+
     }
     file.close();
     return 0;
